e5c0426509b79192de9f029d6b4bb00a4c7eb641
Weezy13090 <wydiane.beddou@laplateforme.io>
2023-04-03 19:52:20 +0200

    8-16

---
+++ jour01/job12.sql
@@ -0,0 +1,3 @@
+USE laplateforme;
+
+INSERT INTO etudiants (nom, prenom, age, email) VALUES ('Dupuis', 'Martin', 18, 'martin.dupuis@laplateforme.io');
---
+++ jour01/job16.sql
@@ -0,0 +1,1 @@
+SELECT * etudiants WHERE prenom LIKE 'b%';
